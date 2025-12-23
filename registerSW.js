a9db24322188fdb3e53d2884255c51fbaf6d6cfd
Subset28 <122221745+Subset28@users.noreply.github.com>
2025-12-23 21:39:18 +0000

    Deploying to gh-pages from @ Armaans-Tech-Tips/armaans-tech-tips.github.io@7a6e9c3f05f0384b9889e4f13b947865cfd19da9 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Anonymous-Tech-Tips/sw.js', { scope: '/Anonymous-Tech-Tips/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
